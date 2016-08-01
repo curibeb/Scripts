93e965a8874a65fc07b7fd1e7dda0332f5a4d8ac
moham <moham@DESKTOP-11R2U1T>
2016-08-02 02:43:29 +0300

    interact players

---
+++ src/scripts/api/entities/InteractPlayer.java
@@ -0,0 +1,22 @@
+package scripts.api.entities;
+
+import org.tribot.api2007.Players;
+import org.tribot.api2007.types.RSPlayer;
+
+public class InteractPlayer {
+
+	private RSPlayer[] players() {
+		return Players.getAll();
+	}
+
+	public boolean interactingWithMe() {
+		RSPlayer[] players = this.players();
+		if (players.length == 0)
+			return false;
+		for (RSPlayer p : players) {
+			return p.isInteractingWithMe();
+		}
+		return false;
+	}
+	
+}
